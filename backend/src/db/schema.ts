c8480df36fe2f2ea7525f273a6948de8695d867b
Rahul Sain <sainrahul374@gmail.com>
2026-09-04 17:28:39 +0530

    feat(backend): created the blogs schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,5 +1,14 @@
 import { sql } from "drizzle-orm";
-import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  jsonb,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+  integer,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id")
@@ -9,6 +18,28 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 255 }).unique().notNull(),
   avatarUrl: text("avatar_url").default("").notNull(),
   googleId: text("google_id").unique().notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull()
+    .$onUpdateFn(() => new Date()),
+});
+
+export const blogs = pgTable("blogs", {
+  id: uuid("id")
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  title: varchar("title", { length: 255 }).notNull(),
+  slug: varchar("slug", { length: 255 }).notNull().unique(),
+  description: text("description"),
+  content: jsonb("content").notNull(),
+  coverImage: text("cover_image"),
+  tags: text("tags").array().notNull().default([]),
+  isPublished: boolean("is_published").default(false).notNull(),
+  publishedAt: timestamp("published_at", { withTimezone: true }),
+  views: integer("views").default(0).notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
     .defaultNow()
     .notNull(),
