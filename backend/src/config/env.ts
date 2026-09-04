22fe93f7c55ed4f4e93011f5cf922c3e06247d70
Rahul Sain <sainrahul374@gmail.com>
2026-09-04 10:58:27 +0530

    feat(backend): setup the env config file

---
+++ backend/src/config/env.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const envSchema = z
+  .object({
+    PORT: z
+      .string()
+      .default("8000")
+      .transform((val) => parseInt(val, 10)),
+    BUN_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+
+    DATABASE_URL: z.url("DATABASE_URL must be a valid connection string"),
+
+    REDIS_URL: z.url("REDIS_URL must be a valid connection string"),
+
+    ACCESS_TOKEN_SECRET_KEY: z.string(),
+    REFRESH_TOKEN_SECRET_KEY: z.string(),
+  })
+  .strict();
+
+const parsedEnv = envSchema.safeParse(Bun.env);
+
+if (!parsedEnv.success) {
+  console.error("Invalid environment variables configuration footprint:");
+
+  const structuredErrors = z.treeifyError(parsedEnv.error);
+
+  console.error(JSON.stringify(structuredErrors, null, 2));
+  process.exit(1);
+}
+
+export const env = parsedEnv.data;
