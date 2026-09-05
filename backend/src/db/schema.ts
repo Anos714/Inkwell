6e9f866f3ba80482ae648cb45999a2b49b2c849f
Rahul Sain <sainrahul374@gmail.com>
2026-09-05 22:29:32 +0530

    feat(backend): added the blog schema and refactor users schema by adding role field

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,4 +1,5 @@
 import { sql } from "drizzle-orm";
+// import { relations } from "drizzle-orm/_relations";
 import {
   boolean,
   jsonb,
@@ -8,7 +9,10 @@ import {
   uuid,
   varchar,
   integer,
+  pgEnum,
 } from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum("role", ["user", "admin"]);
 
 export const users = pgTable("users", {
   id: uuid("id")
@@ -18,6 +22,7 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 255 }).unique().notNull(),
   avatarUrl: text("avatar_url").default("").notNull(),
   googleId: text("google_id").unique().notNull(),
+  role: roleEnum("role").default("user").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
     .defaultNow()
     .notNull(),
@@ -48,3 +53,98 @@ export const blogs = pgTable("blogs", {
     .notNull()
     .$onUpdateFn(() => new Date()),
 });
+
+// export const blogLikes = pgTable(
+//   "blog_likes",
+//   {
+//     userId: uuid("user_id")
+//       .notNull()
+//       .references(() => users.id, {
+//         onDelete: "cascade",
+//       }),
+
+//     blogId: uuid("blog_id")
+//       .notNull()
+//       .references(() => blogs.id, {
+//         onDelete: "cascade",
+//       }),
+
+//     createdAt: timestamp("created_at", {
+//       withTimezone: true,
+//     })
+//       .defaultNow()
+//       .notNull(),
+//   },
+//   (table) => [
+//     primaryKey({
+//       columns: [table.userId, table.blogId],
+//     }),
+//   ],
+// );
+
+// export const blogComments = pgTable("blog_comments", {
+//   id: uuid("id")
+//     .primaryKey()
+//     .default(sql`uuidv7()`),
+
+//   blogId: uuid("blog_id")
+//     .notNull()
+//     .references(() => blogs.id, {
+//       onDelete: "cascade",
+//     }),
+
+//   userId: uuid("user_id")
+//     .notNull()
+//     .references(() => users.id, {
+//       onDelete: "cascade",
+//     }),
+
+//   content: text("content").notNull(),
+
+//   createdAt: timestamp("created_at", {
+//     withTimezone: true,
+//   })
+//     .defaultNow()
+//     .notNull(),
+
+//   updatedAt: timestamp("updated_at", {
+//     withTimezone: true,
+//   })
+//     .defaultNow()
+//     .notNull(),
+// });
+
+// // relations
+// export const usersRelations = relations(users, ({ many }) => ({
+//   blogLikes: many(blogLikes),
+//   blogComments: many(blogComments),
+// }));
+
+// export const blogsRelations = relations(blogs, ({ many }) => ({
+//   likes: many(blogLikes),
+//   comments: many(blogComments),
+// }));
+
+// export const blogLikesRelations = relations(blogLikes, ({ one }) => ({
+//   user: one(users, {
+//     fields: [blogLikes.userId],
+//     references: [users.id],
+//   }),
+
+//   blog: one(blogs, {
+//     fields: [blogLikes.blogId],
+//     references: [blogs.id],
+//   }),
+// }));
+
+// export const blogCommentsRelations = relations(blogComments, ({ one }) => ({
+//   user: one(users, {
+//     fields: [blogComments.userId],
+//     references: [users.id],
+//   }),
+
+//   blog: one(blogs, {
+//     fields: [blogComments.blogId],
+//     references: [blogs.id],
+//   }),
+// }));
