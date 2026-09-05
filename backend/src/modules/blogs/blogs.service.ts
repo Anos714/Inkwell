03c6e19346e796dd3f2e207b3abc05ede35295ab
Rahul Sain <sainrahul374@gmail.com>
2026-09-05 22:30:14 +0530

    feat(backend): completed the crud of blogs api

---
+++ backend/src/modules/blogs/blogs.service.ts
@@ -0,0 +1,51 @@
+import { AppError } from "../../utils/AppError";
+import {
+  createBlog,
+  deleteBlog,
+  findBlogById,
+  IsAdmin,
+  patchBlog,
+} from "./blogs.repository";
+import { CreateBlogInput, PatchBlogInput } from "./blogs.schema";
+
+export const getBlogByIdService = async (blogId: string) => {
+  const blog = await findBlogById(blogId);
+  return blog;
+};
+
+export const createBlogService = async (
+  userId: string,
+  data: CreateBlogInput,
+) => {
+  const user = await IsAdmin(userId);
+  if (!user) {
+    throw AppError.Unauthorized("User not authorized");
+  }
+
+  const blog = await createBlog(data);
+  return blog;
+};
+
+export const patchBlogService = async (
+  userId: string,
+  blogId: string,
+  data: PatchBlogInput,
+) => {
+  const user = await IsAdmin(userId);
+  if (!user) {
+    throw AppError.Unauthorized("User not authorized");
+  }
+
+  const blog = await patchBlog(blogId, data);
+  return blog;
+};
+
+export const deleteBlogService = async (userId: string, blogId: string) => {
+  const user = await IsAdmin(userId);
+  if (!user) {
+    throw AppError.Unauthorized("User not authorized");
+  }
+
+  const blog = await deleteBlog(blogId);
+  return blog;
+};
