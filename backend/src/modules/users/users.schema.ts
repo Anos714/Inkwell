a00e0e2c13bd7574c07df966dac9846e0e634b25
Rahul Sain <sainrahul374@gmail.com>
2026-09-10 12:41:45 +0530

    chore(backend): chnage the regex of user profile update zod schema

--- backend/src/modules/users/users.schema.ts
+++ backend/src/modules/users/users.schema.ts
@@ -14,9 +14,9 @@ export const updateUserProfileSchema = z
       .min(3, "Username must be at least 3 characters")
       .max(30, "Username must be at most 30 characters")
       .regex(
-        /^[a-zA-Z0-9_]+$/,
-        "Username can only contain letters, numbers and underscores",
-      ),
+  /^[a-zA-Z0-9_ ]+$/,
+  "Username can only contain letters, numbers, underscores and spaces",
+)
   })
   .strict();
 
